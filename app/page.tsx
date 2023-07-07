cd2f7d1ff2c708a91a89c0bd78854d2f315def29
Eric Alexander <ekalex08@gmail.com>
2023-07-07 16:51:00 -0400

    added contact and gift card section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
-/*import Contact from "@/components/Contact"; */
+import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
@@ -19,7 +19,8 @@ export default function Home() {
       <ScrollUp />
       <Hero />
       <Features />      
-      <Testimonials />           
+      <Testimonials />
+      <Contact />          
       
     </>
   );
