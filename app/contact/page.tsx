fec48504cdd867ae9ca3f247bd2a2744471bda88
ekalex08 <ekalex08@gmail.com>
2023-07-08 22:36:39 -0400

    w

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,11 @@
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import Contact from "@/components/Contact";
+import { useEffect } from 'react';
 
 const ContactPage = () => {
+  useEffect(() => {
+    // This empty useEffect will mark the parent component as a Client Component
+  }, []);
   return (
     <>
       <Breadcrumb
