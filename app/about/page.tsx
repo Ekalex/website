65683ce4afa8952914423e933012c6220f2239b1
ekalex08 <ekalex08@gmail.com>
2023-07-08 22:40:34 -0400

    hitting a wall

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,13 +2,11 @@ import AboutSectionOne from "@/components/About/AboutSectionOne";
 /*import AboutSectionTwo from "@/components/About/AboutSectionTwo";*/
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import useFirebaseStorage from "@/useFirebaseStorage";
-import { useEffect } from 'react';
+import { useClient } from 'react';
 
 
 const AboutPage = () => {
-  useEffect(() => {
-
-  },[]);
+  useClient();
 
   return (
     
