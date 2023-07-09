b6454dce8d40b1c1dd8939eafda117dc67618737
ekalex08 <ekalex08@gmail.com>
2023-07-08 21:43:43 -0400

    could this be it?

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 /*import AboutSectionTwo from "@/components/About/AboutSectionTwo";*/
 import Breadcrumb from "@/components/Common/Breadcrumb";
+import useFirebaseStorage from "@/useFirebaseStorage";
 
 const AboutPage = () => {
   return (
