2ff1fce0103153908c1b530d6f9f4515a7d20c17
bbelley <brianbelley@gmail.com>
2023-08-05 15:53:23 -0400

    Homepage footer styling and mobile fixes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -8,9 +9,13 @@ const Hero = () => {
         className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
       >
         <div className="container">
-          <div className="-mx-4 flex flex-wrap">
+          <div className="flex flex-wrap">
             <div className="w-full px-4">
-            <img src="/images/logo/png/colorLogo-noBackground.png" ></img>  
+              <Image
+                src="/images/logo/png/colorLogo-noBackground.png"
+                alt="No background logo"
+                width={1168}
+                height={362} />
             </div>
           </div>
         </div>
