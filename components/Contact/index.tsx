36f8df484192c9af10b40d627d1825b8a8f5d60f
Eric Alexander <ekalex08@gmail.com>
2023-11-19 00:10:35 -0500

    customized form

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -11,8 +11,8 @@ const Contact = () => {
     <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" ></div>
   </div>
   <div className="mx-auto max-w-2xl text-center">
-    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-    <p className="mt-2 text-lg leading-8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Setup a Consultation</h2>
+    <p className="mt-2 text-lg leading-8 text-gray-600">Discuss menu options and other details</p>
   </div>
   <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
     <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
@@ -27,13 +27,7 @@ const Contact = () => {
         <div className="mt-2.5">
           <input type="text" name="last-name" id="last-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
         </div>
-      </div>
-      <div className="sm:col-span-2">
-        <label className="block text-sm font-semibold leading-6 text-gray-900">Company</label>
-        <div className="mt-2.5">
-          <input type="text" name="company" id="company" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-        </div>
-      </div>
+      </div>      
       <div className="sm:col-span-2">
         <label className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
         <div className="mt-2.5">
@@ -62,22 +56,11 @@ const Contact = () => {
         <div className="mt-2.5">
           <textarea name="message" id="message" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
         </div>
-      </div>
-      <div className="flex gap-x-4 sm:col-span-2">
-        <div className="flex h-6 items-center">          
-          <button type="button" className="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="false" aria-labelledby="switch-1-label">           
-          
-            <span aria-hidden="true" className="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
-          </button>
-        </div>
-        <label className="text-sm leading-6 text-gray-600" id="switch-1-label">
-          By selecting this, you agree to our
-          <a href="#" className="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
-        </label>
-      </div>
+      </div>     
     </div>
     <div className="mt-10">
-      <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+      <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">submit
+      </button>
     </div>
   </form>
 </div>         
