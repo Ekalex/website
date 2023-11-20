aa1a9f49107aed224f1a372e1160ca935f2e4612
Eric Alexander <ekalex08@gmail.com>
2023-11-20 11:54:32 -0500

    initial commit + style rework

--- components/Testimonials/SingleTestimonial.tsx
+++ components/Testimonials/SingleTestimonial.tsx
@@ -1,10 +1,11 @@
 import { Testimonial } from "@/types/testimonial";
 import Image from "next/image";
+
 const starIcon = (
-  <svg width="18" height="16" viewBox="0 0 18 16" className="fill-current">
+  <svg width="18" height="16" viewBox="0 0 18 16" className="fill-current text-yellow-500">
     <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z" />
   </svg>
-); 
+);
 
 const SingleTestimonial = ({ testimonial }: { testimonial: Testimonial }) => {
   const { star, name, image, content, designation } = testimonial;
@@ -12,26 +13,30 @@ const SingleTestimonial = ({ testimonial }: { testimonial: Testimonial }) => {
   let ratingIcons = [];
   for (let index = 0; index < star; index++) {
     ratingIcons.push(
-      <span key={index} className="text-yellow">
-       
+      <span key={index} className="text-primary">
+        {starIcon}
       </span>
     );
   }
 
   return (
-    <div className="w-full">
-      <div
-        className="wow fadeInUp rounded-md bg-primary p-8 shadow-one lg:px-5 xl:px-8"        
-      >
+    <div className="w-full p-4 transition duration-300 ease-in-out transform hover:scale-105">
+      <div className="rounded-md bg-gradient-to-b from-Charcoal to-Gray bg-opacity-20 p-8 shadow-lg">
+        {image && <Image 
+    src={image} 
+    alt={name} 
+    className="rounded-full" 
+    width={50} // Adjust this value based on your image's size
+    height={50} // Adjust this value based on your image's size 
+    />}
         <div className="mb-5 flex items-center space-x-1">{ratingIcons}</div>
-        <p className="mb-8 border-b border-body-color border-opacity-10 pb-8 text-base leading-relaxed text-body-color text-Charcoal dark:border-Gray dark:border-opacity-10">
-          “{content}
+        <p className="mb-8 border-b border-Charcoal pb-8 text-base leading-relaxed text-body-color">
+          “{content}”
         </p>
-        <div className="flex items-center">          
+        <div className="flex items-center">
           <div className="w-full">
-            <h5 className="mb-1 text-lg font-semibold lg:text-base text-Charcoal xl:text-lg">
-              {name}
-            </h5>             
+            <h5 className="mb-1 text-lg font-semibold text-Charcoal">{name}</h5>
+            <p className="text-sm text-secondary">{designation}</p>
           </div>
         </div>
       </div>
