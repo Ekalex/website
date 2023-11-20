bdd99d957551e95b8ef49a2cb4312c93813315df
Eric Alexander <ekalex08@gmail.com>
2023-11-20 14:52:35 -0500

    trust pilot reviews are loaded in

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,26 +1,11 @@
-import NewsLatterBox from "./NewsLatterBox";
-import Image from "next/image";
-
 const Contact = () => {
   return (
-    <section className="mb-32 text-center lg:text-left">
-      <div className="px-6 py-12 md:px-12">
-        <div className="grid items-center lg:grid-cols-2 lg:gap-x-12 p-10px">
-          <div className="mb-12 lg:mb-0">
-            <h2 className="my-12 text-5xl font-bold leading-tight tracking-tight">
-              Are you ready <br />
-              <span className="text-success dark:text-success-400">to get started?</span>
-            </h2>
-            <a className="mb-2 inline-block rounded m-4 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-gray-600 bg-primary hover:bg-indigo-400 hover:text-white"
-              data-te-ripple-init data-te-ripple-color="light" href="mailto:eric@onthymepersonalchef.com" role="button">Schedule a consultation</a>
-            <a className="inline-block rounded  m-4 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-gray-600 bg-primary hover:bg-indigo-400 hover:text-white"
-              data-te-ripple-init data-te-ripple-color="light" href="https://squareup.com/gift/MLSQND4VMTYVV/order" role="button">Give the perfect gift</a>
-          </div>          
-        </div>
+    <section className="mb-32 text-center lg:text-left flex justify-center">
+      <div className="w-full max-w-3xl p-4"> {/* Adjust width as needed */}
+        <div data-tf-live="01HFQ1XQVYFQJ9TGVE8KPPCQFG"></div>
+        <script src="//embed.typeform.com/next/embed.js"></script>
       </div>
     </section>
-
-
   );
 };
 
--- components/Testimonials/index.tsx
+++ components/Testimonials/index.tsx
@@ -6,7 +6,7 @@ const testimonialData: Testimonial[] = [
   {
     id: 1,
     name: " Max and Merrill Barrett",
-    designation: "Previous Clients",
+    designation: "Former Clients",
     content:
       "We appreciate all of the help that Eric did for us. He is kind, friendly, and his food is amazing! He was just the best!'",
     image: "/images/testimonials/auth-01.png",
@@ -15,7 +15,7 @@ const testimonialData: Testimonial[] = [
   {
     id: 2,
     name: "The Spaulding Family",
-    designation: "Previous Clients",
+    designation: "Current Clients",
     content:
       "Eric has been cooking for us for several months now, and we are thrilled with his service. He is professional, punctual, and talented. With two toddlers and lots of different food preferences in the house, he has managed to find meals that all of us love. And it has been lovely to have some additional help in the dinner department with another little one on the way. He also leaves the kitchen clean and all the meals neatly put away which makes it so easy for the rest of the week. We highly recommend Eric and his services.'",
     image: "/images/testimonials/auth-02.png",
@@ -23,8 +23,8 @@ const testimonialData: Testimonial[] = [
   },
   {
     id: 3,
-    name: "Alok Solomani",
-    designation: "Previous Clients",
+    name: "Nico Drillis",
+    designation: "Client",
     content:
       "We hired Eric to cook for a 50th birthday party.  He is organized and easy to work with.  He crafted a great menu and cheerfully accommodated last minute requests.  The execution was spot on - he shopped, planned, cooked, plated and delivered to perfection. We highly recommend.'",
     image: "/images/testimonials/auth-03.png",
@@ -32,41 +32,51 @@ const testimonialData: Testimonial[] = [
   },
 
   {
-    id: 3,
-    name: "Alok Solomani",
-    designation: "Previous Clients",
+    id: 4,
+    name: "Kari Borroel",
+    designation: "Client",
     content:
-      "We hired Eric to cook for a 50th birthday party.  He is organized and easy to work with.  He crafted a great menu and cheerfully accommodated last minute requests.  The execution was spot on - he shopped, planned, cooked, plated and delivered to perfection. We highly recommend.'",
+    "Eric has been preparing meals for my husband and I for several months, and it has been great! He is an excellent chef, very open to preferences and requests, and he cooks with a lot of variety. Eric has helped us to eat better and healthier with our busy work and lives. I would absolutely recommend Eric to cook for you!",
     image: "/images/testimonials/auth-03.png",
     star: 5,
   },
 
   {
-    id: 3,
-    name: "Alok Solomani",
-    designation: "Previous Clients",
+    id: 5,
+    name: "The Shaw Family",
+    designation: "Former Clients",
     content:
-      "We hired Eric to cook for a 50th birthday party.  He is organized and easy to work with.  He crafted a great menu and cheerfully accommodated last minute requests.  The execution was spot on - he shopped, planned, cooked, plated and delivered to perfection. We highly recommend.'",
+      "I’ve had the pleasure to hire Eric on multiple occasions, and he never disappoints! I’m a full time working mom and and wife and the last thing I want to do is come home and think what to make for dinner each night. Eric has been a life saver and has crossed this task off my to do list! He also has made mornings a breeze, my children (and especially me) love his sous vide egg bites! Eric goes above and beyond to make sure our entire family is happy with the menus each week. Currently Eric is putting together a menu for a private 40th event next month, and I cannot wait for him to wow my guests!",
     image: "/images/testimonials/auth-03.png",
     star: 5,
   },
 
   {
-    id: 3,
-    name: "Alok Solomani",
-    designation: "Previous Clients",
+    id: 6,
+    name: "Brian Belley",
+    designation: "Client",
     content:
-      "We hired Eric to cook for a 50th birthday party.  He is organized and easy to work with.  He crafted a great menu and cheerfully accommodated last minute requests.  The execution was spot on - he shopped, planned, cooked, plated and delivered to perfection. We highly recommend.'",
+      "We recently had Eric come to our home and had a 4-course autumn tasting menu, with a different wine pairing for each of the four courses. We invited another couple so there were four of us for this amazing experience. Every detail of the evening was taken care of for us by Eric - from the INCREDIBLE food, to the setting of the table and service for each of the courses. Eric and his team even took care of dishes and cleaning, so we had a spotless kitchen and didn't have to do any dishes or take out any trash! It was readily apparent that Eric is very organized, experienced, and came with a very friendly attitude and was willing to answer our questions. He was very prepared and thorough leading up to the evening, and it paid off as it went off flawlessly. Will definitely be looking to do this again sometime soon!",
     image: "/images/testimonials/auth-03.png",
     star: 5,
   },
 
   {
-    id: 3,
-    name: "Alok Solomani",
-    designation: "Previous Clients",
+    id: 7,
+    name: "Carolyn K",
+    designation: "Former Client",
     content:
-      "We hired Eric to cook for a 50th birthday party.  He is organized and easy to work with.  He crafted a great menu and cheerfully accommodated last minute requests.  The execution was spot on - he shopped, planned, cooked, plated and delivered to perfection. We highly recommend.'",
+      "Eric did an amazing job cooking dinner for our family each week! From beef stew to chicken pot pies, the food was delicious and made life super easy for two very busy professionals with two very busy kids! Can not recommend Eric enough. Reasonably priced and very talented!",
+    image: "/images/testimonials/auth-03.png",
+    star: 5,
+  },
+
+  {
+    id: 8,
+    name: "Alok Solomani",
+    designation: "Client",
+    content:
+      "We wanted something a bit special for my wife’s ?0 birthday and Eric really hit it out of the park for us. From initial contact through to clean up and goodbyes, Eric was professional, efficient and responsive to our requests. Plus, he totally noticed the interest of the teenagers and involved them which was awesome. And the dinner itself - from starters through dessert - huge hit with us and our guests. It was well presented and delicious. We highly recommend Eric and his team.",
     image: "/images/testimonials/auth-03.png",
     star: 5,
   },
