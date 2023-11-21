5485a154cff0952e2eab31fd21d620150a72d244
Eric Alexander <ekalex08@gmail.com>
2023-11-21 12:39:20 -0500

    deploying

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,11 +1,12 @@
+import React, { useEffect } from 'react';
+
 const Contact = () => {
   return (
-    <section className="mb-32 text-center lg:text-left flex justify-center">
-      <div className="w-full max-w-3xl p-4"> {/* Adjust width as needed */}
-        <div data-tf-live="01HFQ1XQVYFQJ9TGVE8KPPCQFG"></div>
-        <script src="//embed.typeform.com/next/embed.js"></script>
-      </div>
+    <section className="mb-32 text-center lg:text-left">
+<div data-tf-live="01HFQ1XQVYFQJ9TGVE8KPPCQFG"></div><script src="//embed.typeform.com/next/embed.js"></script>
     </section>
+
+
   );
 };
 
