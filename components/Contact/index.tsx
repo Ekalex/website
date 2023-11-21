48e9c5c507ca7c5efad913abfd478710ddc4fc31
Eric Alexander <ekalex08@gmail.com>
2023-11-21 14:02:28 -0500

    rework of contact section

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -8,12 +8,11 @@ const Contact = () => {
         <div className="grid items-center lg:grid-cols-2 lg:gap-x-12 p-10px">
           <div className="mb-12 lg:mb-0">
             <h2 className="my-12 text-5xl font-bold leading-tight tracking-tight">
-              Are you ready <br />
-              <span className="text-success dark:text-success-400">to get started?</span>
+              Lets Chat!<br />              
             </h2>
-            <a className="mb-2 inline-block rounded m-4 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-gray-600 bg-primary hover:bg-indigo-400 hover:text-white"
-              data-te-ripple-init data-te-ripple-color="light" href="mailto:eric@onthymepersonalchef.com" role="button">Schedule a consultation</a>
-            <a className="inline-block rounded  m-4 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-gray-600 bg-primary hover:bg-indigo-400 hover:text-white"
+            <a className="mb-2 inline-block rounded m-4 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-Charcoal bg-primary hover:bg-Charcoal hover:text-primary"
+              data-te-ripple-init data-te-ripple-color="light" href="https://qa5h1fa5sob.typeform.com/to/lbG0R544" role="button" target="blank">Schedule a consultation</a>
+            <a className="inline-block rounded  m-4 px-12 pt-4 pb-3.5 text-sm font-medium uppercase leading-normal text-gray-600 bg-primary hover:bg-Charcoal hover:text-primary"
               data-te-ripple-init data-te-ripple-color="light" href="https://squareup.com/gift/MLSQND4VMTYVV/order" role="button">Give the perfect gift</a>
           </div>          
         </div>
