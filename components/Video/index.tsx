2f602434d75bf1db7317a853e516287e5d4df814
Eric Alexander <ekalex08@gmail.com>
2023-11-21 15:23:45 -0500

    Gallery linked to firebase storage

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,110 +1,48 @@
-"use client";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
-import Image from "next/image";
-import { useState } from "react";
-import SectionTitle from "../Common/SectionTitle";
+// Initialize Firebase
+const firebaseConfig = {
+  // your firebase config
+};
 
-
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 
 const Video = () => {  
+  const [images, setImages] = useState([]);
 
-  return (    
+  useEffect(() => {
+    console.log("Component mounted")
+    const fetchImages = async () => {
+      const storageRef = ref(storage, 'gs://website-211bf.appspot.com/gallery');
+
+      try {
+        const imageRefs = await listAll(storageRef);
+        const imageUrls = await Promise.all(
+          imageRefs.items.map((item) => getDownloadURL(item))
+        );
+        setImages(imageUrls);
+      } catch (error) {
+        console.error("Error fetching images: ", error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  return (
     <div className="container mx-auto p-4">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://upload.wikimedia.org/wikipedia/commons/0/03/Kashi_Vishwanath_Temple_Banaras.jpg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://pbs.twimg.com/media/FGRnUzPVEAAbqM8?format=jpg&name=large"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://pbs.twimg.com/media/FGRnNpAVUAYqRYv?format=jpg&name=large"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://pbs.twimg.com/media/FGRnP_TUUAAttG3?format=jpg&name=large"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://i.pinimg.com/originals/c0/7d/17/c07d17d7ca0b9f39f5aded4b6dca8f02.jpg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Benares-_The_Golden_Temple%2C_India%2C_ca._1915_%28IMP-CSCNWW33-OS14-66%29.jpg/1280px-Benares-_The_Golden_Temple%2C_India%2C_ca._1915_%28IMP-CSCNWW33-OS14-66%29.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://www.jagranimages.com/images/newimg/27072020/27_07_2020-shri-kashi-vishwanath-temple_20557350.jpg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://www.jansatta.com/wp-content/uploads/2021/12/Kashi-Vishwanath-Mandir.png?w=1024"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://staticimg.amarujala.com/assets/images/2021/11/07/750x506/kashi-vishwanath-dham_1636258507.jpeg?w=674&dpr=1.0"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://staticimg.amarujala.com/assets/images/2020/01/13/750x506/kashi-vishwanath-mandir-varanasi_1578924152.png?w=700&dpr=2.0"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Ahilya_Ghat_by_the_Ganges%2C_Varanasi.jpg/800px-Ahilya_Ghat_by_the_Ganges%2C_Varanasi.jpg"
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg"
-                  src="https://upload.wikimedia.org/wikipedia/commons/2/25/Chet_Singh_Ghat_in_Varanasi.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        {images.map((url, index) => (
+          <div key={index} className="grid gap-4">
+            <img className="h-auto max-w-full rounded-lg" src={url} alt="" />
           </div>
-        </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
