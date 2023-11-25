5d4d352fd0d7643833a882e7bc0706f98f65205a
Eric Alexander <ekalex08@gmail.com>
2023-11-25 14:33:01 -0500

    couple fixes

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
+
 const Footer = () => {
   const [logoUrl, setLogoUrl] = useState('');
   const [insuranceSealUrl, setInsuranceSealUrl] = useState('');
---
+++ firebaseconfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "gs://website-211bf.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+
+export const storage = firebase.storage();
