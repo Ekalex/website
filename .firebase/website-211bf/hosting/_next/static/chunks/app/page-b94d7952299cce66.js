bb0245d1542271c06b3556bf53d40a8a567b740e
Eric Alexander <ekalex08@gmail.com>
2023-11-25 22:40:08 -0500

    more broken links

---
+++ .firebase/website-211bf/hosting/_next/static/chunks/app/page-b94d7952299cce66.js
@@ -0,0 +1,16 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9792:function(e,r,n){Promise.resolve().then(n.bind(n,5766)),Promise.resolve().then(n.bind(n,3802)),Promise.resolve().then(n.t.bind(n,6507,23)),Promise.resolve().then(n.t.bind(n,30,23))},5766:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return o}});var t=n(5784);function o(){return(0,t.useEffect)(()=>{var e;return null===(e=window.document.scrollingElement)||void 0===e?void 0:e.scrollTo(0,0)},[]),null}},3802:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return d}});var t=n(1844),o=n(5784),s=n(7779);/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */(0,s.KN)("firebase","10.6.0","app");var l=n(6650);let a=(0,s.ZF)({}),i=(0,l.cF)(a),c=()=>{let[e,r]=(0,o.useState)([]);return(0,o.useEffect)(()=>{console.log("Component mounted");let e=async()=>{let e=(0,l.iH)(i,"gs://website-211bf.appspot.com/gallery");try{let n=await (0,l.aF)(e),t=await Promise.all(n.items.map(e=>(0,l.Jt)(e)));r(t)}catch(e){console.error("Error fetching images: ",e)}};e()},[]),(0,t.jsx)("div",{className:"container p-4",children:(0,t.jsx)("div",{className:"grid grid-cols-2 md:grid-cols-4 gap-4",children:e.map((e,r)=>(0,t.jsx)("div",{className:"flex flex-col gap-4",children:(0,t.jsx)("div",{className:"hover:border-primary transform transition duration-500 hover:scale-105 hover:shadow-lg",children:(0,t.jsx)("img",{className:"block h-auto max-w-full rounded-lg border border-gray-300 bg-Gray",src:e,alt:""})})},r))})})};var d=c}},function(e){e.O(0,[30,806,17,744],function(){return e(e.s=9792)}),_N_E=e.O()}]);
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
-
 const Footer = () => {
   const [logoUrl, setLogoUrl] = useState('');
   const [insuranceSealUrl, setInsuranceSealUrl] = useState('');
@@ -44,29 +43,29 @@ const Footer = () => {
           <div className="flex flex-col items-center mb-12 lg:mb-16">
             <Link href="/" legacyBehavior>
               <a className="mb-8">
-                <Image 
-                  src="https://firebasestorage.googleapis.com/v0/b/website-211bf.appspot.com/o/colorLogo-noBackground.png?alt=media&token=83d207a8-5003-4cb5-8a62-f88bddbd5b77" 
+                {logoUrl && <Image 
+                  src={logoUrl} 
                   alt="logo" 
                   width={280} 
                   height={60}
                   className="dark:block" 
-                />
+                />}
               </a>
             </Link>
             <div className="flex">
               {/* Social Media Icons Here */}
-              <a href="https://www.facebook.com/profile.php?id=100093953964804" aria-label="facebook" className="mr-6">
-                <Image src="https://firebasestorage.googleapis.com/v0/b/website-211bf.appspot.com/o/icons%2Ficons8-facebook.svg?alt=media&token=fb577d60-5771-4ae0-949f-9528987f30d8" alt="Facebook" width={16} height={16} />
-              </a>
-              <a href="https://www.instagram.com/onthymepersonalchef/" aria-label="instagram" className="mr-6">
-                <Image src="https://firebasestorage.googleapis.com/v0/b/website-211bf.appspot.com/o/icons%2Ficons8-instagram.svg?alt=media&token=e0135163-75df-4e5f-acbe-ef335e77ac83" alt="Instagram" width={16} height={16} />
-              </a>
-              <a href="https://www.linkedin.com/in/eric-alexander-08289665/" aria-label="linkedIn" className="mr-6">
-                <Image src="https://firebasestorage.googleapis.com/v0/b/website-211bf.appspot.com/o/icons%2Ficons8-linked-in.svg?alt=media&token=045c70c8-7775-4104-90d4-7121a1aded68" alt="LinkedIn" width={16} height={16} />
-              </a>
-              <a href="mailto:eric@onthymepersonalchef.com" aria-label="email" className="mr-6">
-                <Image src="https://firebasestorage.googleapis.com/v0/b/website-211bf.appspot.com/o/icons%2Ficons8-email.svg?alt=media&token=d7b7b229-44b5-4535-857a-67628cc10e9d" alt="Email" width={16} height={16} />
-              </a>
+              {facebookIconUrl && <a href="https://www.facebook.com/profile.php?id=100093953964804" aria-label="facebook" className="mr-6">
+                <Image src={facebookIconUrl} alt="Facebook" width={16} height={16} />
+              </a>}
+              {instagramIconUrl && <a href="https://www.instagram.com/onthymepersonalchef/" aria-label="instagram" className="mr-6">
+                <Image src={instagramIconUrl} alt="Instagram" width={16} height={16} />
+              </a>}
+              {linkedInIconUrl && <a href="https://www.linkedin.com/in/eric-alexander-08289665/" aria-label="linkedIn" className="mr-6">
+                <Image src={linkedInIconUrl} alt="LinkedIn" width={16} height={16} />
+              </a>}
+              {emailIconUrl && <a href="mailto:eric@onthymepersonalchef.com" aria-label="email" className="mr-6">
+                <Image src={emailIconUrl} alt="Email" width={16} height={16} />
+              </a>}
             </div>
           </div>
 
@@ -74,13 +73,13 @@ const Footer = () => {
           <div className="w-full sm:w-1/2 lg:w-2/12">
             <Link href="http://www.fliprogram.com" legacyBehavior>
               <a className="flex justify-center">
-                <Image 
-                  src="https://firebasestorage.googleapis.com/v0/b/website-211bf.appspot.com/o/icons%2FFLIP_insurance_seal.png?alt=media&token=37365077-dd38-40d8-82d3-f9ed795a1217" 
+                {insuranceSealUrl && <Image 
+                  src={insuranceSealUrl} 
                   alt="FLIP Insurance Seal" 
                   width={256} 
                   height={256} 
                   className="bg-white rounded-lg" 
-                />
+                />}
               </a>
             </Link>
           </div>
